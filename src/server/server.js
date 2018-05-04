b1a3e7ae333601144c88c670774d34cf56831f3b
npatrick <wazzupnickz@gmail.com>
2018-05-04 11:28:19 -0700

    add bing crawling

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,5 @@
 'use strict';
-require('dotenv').config();
+
 const express = require('express');
 const logger = require('morgan');
 const rp = require('request-promise');
@@ -7,37 +7,66 @@ const cheerio = require('cheerio');
 const Nightmare = require('nightmare');
 const User = require('../db/userSchema');
 const vo = require('vo');
+const mongoose = require('mongoose');
+const db = require('../db/index');
 
 const app = express();
+
 app.use(logger('dev'));
 
-app.get('/crawl', (req, res) => {
+db.on('open', () => {
   const googleQuery = 'https://www.google.com/search?q=site:www.instagram.com+%22los+angeles%22+LA+blogger';
-  const max = 30;
+  const gMax = 32;
   let segment;
 
   const reqPromCrawl = require('./controllers/reqPromCrawl');
 
   let searchArr = [];
 
-  for (let k = 0; k < 3; k++) {
-    segment = `&start=${k * 10}&sa=N`;
-    searchArr.push(googleQuery + segment);
+  // for (let k = 0; k < gMax; k++) {
+  //   segment = `&start=${k * 10}&sa=N`;
+  //   searchArr.push(googleQuery + segment);
+  // }
+
+  // console.log('LISTINGS: \n', searchArr);
+
+  // searchArr.reduce(function(accumulator, url) {
+  //   return accumulator.then(function(results) {
+  //     return reqPromCrawl(url);
+  //   });
+  // }, Promise.resolve([])).then(function(results) {
+  //   console.log('Google query items ends...', results);
+  // });
+
+  // searchArr = [];
+
+  const bingQuery = 'https://www2.bing.com/search?q=site%3ainstagram.com+"Los+Angeles"+LA+blogger';
+  const bMax = 72;
+  let pageSector;
+
+  for (let j = 0; j < bMax; j++) {
+    if (j === 0) {
+      pageSector = `&first=1`;
+    } else if (j === 1) {
+      pageSector = `&first=13`;
+    } else {
+      pageSector = `&first=${(14 * j) - 1}`;
+    }
+    searchArr.push(bingQuery + pageSector);
   }
 
-  console.log('LISTINGS: \n', searchArr);
+  console.log('BING LISTINGS', searchArr);
 
   searchArr.reduce(function(accumulator, url) {
     return accumulator.then(function(results) {
       return reqPromCrawl(url);
     });
   }, Promise.resolve([])).then(function(results) {
-    console.log('hmmmmmmm?', results);
-    res.end();
+    console.log('Bing query items ends...', results);
   });
 
   searchArr = [];
-
+  
 })
 
 module.exports = app;
