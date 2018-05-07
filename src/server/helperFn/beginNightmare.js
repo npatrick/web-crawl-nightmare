ad479d2fa2fffcd8489210736fd4931e15680fb1
npatrick <wazzupnickz@gmail.com>
2018-05-07 13:18:03 -0700

    add blacklist of sites to avoid scraping

--- src/server/helperFn/beginNightmare.js
+++ src/server/helperFn/beginNightmare.js
@@ -5,16 +5,16 @@ const nightmare = Nightmare({
   show: true,
   // gotoTimeout => def:30s, 
   // only used if the DOM itself has not yet loaded
-  gotoTimeout: 60000,
+  gotoTimeout: 30000,
   // waitTimeout => def:30s, throws an exception if .wait()
   // didnt return true within the set timeframe
-  waitTimeout: 60000,
+  waitTimeout: 30000,
   // executionTimeout => def:30s, max time to wait for an
   // .evaluate() statement to complete
-  executionTimeout: 60000,
+  executionTimeout: 30000,
   // pollInterval => def:250ms, wait time between checks
   // for the .wait() condition to be successful
-  pollInterval: 500,
+  pollInterval: 50,
   // switches => command line switches used by Chrome
   // and supported by Electron
   switches: {
@@ -35,27 +35,42 @@ const nightmare = Nightmare({
 const beginNightmare = (domain, selectorStr, isUserWeb) => {
     let waitTimer = isUserWeb ? 1 : 2000;
   	let normalizeDomain = '';
-  	if (!domain.includes('http')) {
-  		normalizeDomain = `http://${domain}`;
-  	} else {
-  		normalizeDomain = domain;
-  	}
-    console.log('now checking in ', normalizeDomain);
-		return nightmare
-      .goto(normalizeDomain)
-      .wait(waitTimer)
-    	.wait(selectorStr)
-      .evaluate((selector) => {
-        return window.document.querySelector(selector).outerHTML;
-      }, selectorStr)
-      .then((el) => {
-        const $ = cheerio.load(el);
-        return $;
-      })
-      .catch(error => {
-        console.log(`Execution failed on beginNightmare fn for ${normalizeDomain}\n Error stat:`, error);
-        return;
-      })
+    let siteBlacklisted = false;
+    const blackList = ['vogue.com', 'consumingla.com', 'okayafrica.com'];
+    blackList.forEach((url) => {
+      if (domain.toLowerCase().includes(url)) {
+        siteBlacklisted = true;
+      }
+    })
+    if (!siteBlacklisted) {
+    	if (!domain.includes('http')) {
+    		normalizeDomain = `http://${domain}`;
+    	} else {
+    		normalizeDomain = domain;
+    	}
+      console.log('now checking in ', normalizeDomain);
+  		return nightmare
+        .goto(normalizeDomain)
+        .wait(waitTimer)
+      	.wait(selectorStr)
+        .evaluate((selector) => {
+          return window.document.querySelector(selector).outerHTML;
+        }, selectorStr)
+        .then((el) => {
+          const $ = cheerio.load(el);
+          return $;
+        })
+        .catch(error => {
+          console.log(`Execution failed on beginNightmare fn for ${normalizeDomain}\n Error stat:`, error);
+          if (error.details == 'Navigation timed out after 60000 ms') {
+            console.log('I got error details, seeeeee =>', error.details);
+            return beginNightmare;
+          }
+          return;
+        })  
+    }
+    console.log('BLACKLISTED:', domain);
+    return nightmare;
   };
 
 module.exports = beginNightmare;
