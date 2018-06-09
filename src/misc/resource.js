0b777b7a68d83442045ec45ebfadb3bee058cb73
npatrick <wazzupnickz@gmail.com>
2018-06-09 15:38:14 -0700

    add more keywords to detect

--- src/misc/resource.js
+++ src/misc/resource.js
@@ -1,5 +1,5 @@
 module.exports.keyword = ['food', 'foodie', 'style', 'fashion', 'beauty', 'makeup', 'stylist', 'lifestyle',
-                'author', 'travel', 'adventure', 'adventurer', 'clothing', 'news', 'film', 'cinema', 'blog', 'blogger',
-                'vlog', 'influencer', 'model', 'nutrition', 'fitness', 'wellness', 'home', 'kitchen'];
+                'author', 'travel', 'traveler', 'adventure', 'adventurer', 'clothing', 'news', 'film', 'cinema', 'blog', 'blogger',
+                'vlog', 'influencer', 'model', 'nutrition', 'photographer', 'fitness', 'wellness', 'home', 'kitchen'];
 
 module.exports.topDomain = ['.com', '.net', '.org', '.biz', '.fr', '.info', '.media', '.global', '.email', '.ly', '.us', '.nu', '.ch'];
