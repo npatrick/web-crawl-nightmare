ef66729ce33589e7bd404cbc07f512b89c3ae2ba
npatrick <wazzupnickz@gmail.com>
2018-06-15 13:44:24 -0700

    add another top domain to consider adding for email

--- src/misc/resource.js
+++ src/misc/resource.js
@@ -2,7 +2,7 @@ module.exports.keyword = ['food', 'foodie', 'style', 'fashion', 'beauty', 'makeu
                 'author', 'travel', 'traveler', 'adventure', 'adventurer', 'clothing', 'news', 'film', 'cinema', 'blog', 'blogger',
                 'vlog', 'influencer', 'model', 'nutrition', 'photographer', 'fitness', 'wellness', 'home', 'kitchen'];
 
-module.exports.topDomain = ['.com', '.net', '.org', '.biz', '.fr', '.info', '.media', '.global', '.email', '.ly', '.us', '.nu', '.ch', '.xyz'];
+module.exports.topDomain = ['.com', '.net', '.org', '.biz', '.fr', '.info', '.media', '.global', '.email', '.ly', '.us', '.nu', '.ch', '.xyz', '.it'];
 
 module.exports.blackList = ['instagram.com/about', 'instagram.com/blog', 'vogue.com', 'vogue', 'consumingla.com', 'okayafrica.com', 
                       'amodelsguide.com', 'theclothing-twpm.com', 'amazon.com',
