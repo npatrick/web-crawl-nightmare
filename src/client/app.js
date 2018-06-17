cccddd2dd8cfbdfce1dd74fb1b2900f31a641b54
npatrick <wazzupnickz@gmail.com>
2018-06-17 01:15:04 -0700

    add basic structure of app.js file

--- src/client/app.js
+++ src/client/app.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
 	render() {
 		return (
 			<div className="App">
-				<h1>Hello, World!</h1>
+				<h2>Add query for crawling</h2>
+				<form>
+				  <label>
+				    Search Engine:
+				  </label>
+			    <select class="searchEngine">
+			    	<option value>Google</option>
+			    	<option>Bing</option>
+			    </select>
+			    <br />
+				  <label>
+				  Query:
+				  </label>
+				  <input type="text" name="userQuery"></input>
+				  <input type="submit" value="Submit" />
+				</form>
 			</div>
 		)
 	}
