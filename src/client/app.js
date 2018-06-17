54af0ec7b3764564b154f1921bca8cce93bb4ee1
npatrick <wazzupnickz@gmail.com>
2018-06-17 02:46:03 -0700

    add query post

--- src/client/app.js
+++ src/client/app.js
@@ -1,26 +1,75 @@
 import React, { Component } from 'react';
+import { post as POST } from 'axios';
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			apiMessage: '',
+			userQuery: '',
+			searchEngine: '' // TODO: assign changes on this state
+		}
+
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleEngine = this.handleEngine.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleSubmit(e) {
+		e.preventDefault();
+		const { searchEngine, userQuery } = this.state;
+		let encodeQuote;
+		let doubleQuote;
+		let normQ;
+
+		// removals
+		encodeQuote = encodeURI(userQuery);
+		normQ = encodeQuote.replace(/\'/g, '%27');
+
+		POST('/add-query', {
+			searchEngine: searchEngine,
+			userQuery: normQ
+		})
+		.then(res => this.setState({ apiMessage: res.data }))
+		.catch(err => console.log('error in /add-query', err))
+	}
+
+	handleEngine(e) {
+		this.setState({ searchEngine: e.target.value });
+	}
+
+	handleChange(e) {
+		this.setState({ userQuery: e.target.value });
+	}
 
 	render() {
 		return (
 			<div className="App">
 				<h2>Add query for crawling</h2>
-				<form>
+				<form onSubmit={this.handleSubmit}>
 				  <label>
 				    Search Engine:
 				  </label>
-			    <select class="searchEngine">
-			    	<option value>Google</option>
+			    <select className="searchEngine" name="searchEngine" onChange={this.handleEngine}>
+			    	<option value>Please choose one</option>
+			    	<option>Google</option>
 			    	<option>Bing</option>
 			    </select>
 			    <br />
 				  <label>
 				  Query:
 				  </label>
-				  <input type="text" name="userQuery"></input>
+				  <input type="text" name="userQuery" onChange={this.handleChange}></input>
+				  <br />
 				  <input type="submit" value="Submit" />
 				</form>
+				<br />
+				<br />
+				<div className="apiMessage">
+					<h3>Server Response:</h3>
+					{this.state.apiMessage}
+				</div>
 			</div>
 		)
 	}
