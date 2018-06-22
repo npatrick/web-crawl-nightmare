a832fd71d781bedaf7eb1efab8393075c965148f
npatrick <wazzupnickz@gmail.com>
2018-06-22 13:33:13 -0700

    add more button styling

--- src/client/components/Button.js
+++ src/client/components/Button.js
@@ -1,13 +1,22 @@
 import styled from 'styled-components';
 
 export default styled.button`
+	position: relative;
+	right: ${(props) => props.right};
 	background-color: white;
 	border-color: palevioletred;
 	border-width: 1px;
+	margin-left: 10px;
+	margin-right: 10px;
+	float: ${(props) => (props.float === 'right' ? 'right' : 'left')};
 	color: palevioletred;
 	padding: 5px 10px;
 	text-align: center;
 	text-decoration: none;
 	font-size: 12px;
 	border-radius: 2px;
+
+	&:hover {
+    background: papayawhip;
+  }
 `;
