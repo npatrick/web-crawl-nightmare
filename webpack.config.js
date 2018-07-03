5f1f94715592f7e4877377d6bb18fa7746107644
npatrick <wazzupnickz@gmail.com>
2018-07-03 16:21:53 -0700

    add setup for imagemin plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ImageminPlugin = require('imagemin-webpack-plugin').default
 
 module.exports = {
 	entry: ['./src/client/index.js'],
@@ -10,6 +11,14 @@ module.exports = {
 	resolve: {
 		extensions: ['.js', '.jsx', '.css', '.png', '.jpg', '.gif', 'jpeg']
 	},
+	plugins: [
+		// Use the default settings for everything in /assets/*
+    new ImageminPlugin({ test: 'assets/**' }),
+    // Make sure that the plugin is after any plugins that add images
+    new ImageminPlugin({
+      disable: process.env.NODE_ENV !== 'production', // Disable during development
+    })
+  ],
 	module: {
 		rules: [
 			{
